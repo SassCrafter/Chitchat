70c3ccf67d61f6b2a70c22b7874576fef4b8d26f
Dima <webbootcamp2020@gmail.com>
2021-05-13 23:06:02 +0300

    Link to sign up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { SIGN_UP } from "../constants/routes";
 
 function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Home</h1>
+			<Link to={SIGN_UP}>Sign up</Link>
+		</div>
+	);
 }
 
 export default Home;
